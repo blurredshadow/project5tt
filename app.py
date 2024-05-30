ee0d8e2a03d182aff251a843671cbf40f04541f4
blurredshadow <jfersortre@gmail.com>
2024-05-30 14:49:52 -0600

    se modifica archivo con la identacion correcta

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import pandas as pd
-        import plotly.express as px
-        import streamlit as st
+import plotly.express as px
+import streamlit as st
         
-        car_data = pd.read_csv('C:\\Users\Fernando Soria\\repositorios clonados\\Project-Sprint-5\\vehicles_us.csv') # leer los datos
-        hist_button = st.button('Construir histograma') # crear un botón
+car_data = pd.read_csv('C:\\Users\Fernando Soria\\repositorios clonados\\Project-Sprint-5\\vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
         
-        if hist_button: # al hacer clic en el botón
+if hist_button: # al hacer clic en el botón
             # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
             # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer")
         
             # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
 import streamlit as st
 
